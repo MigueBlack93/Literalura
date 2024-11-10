401c7b633ec9fc10dbdbac2770377aaf0edb4e7d
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-10 19:45:28 -0300

    Create DatosLibro.java

---
+++ src/main/java/com/miguel/literalura/model/DatosLibro.java
@@ -0,0 +1,17 @@
+package com.miguel.literalura.model;
+
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibro(
+        @JsonAlias("id") Integer id,
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") List<DatosAutores> autores,
+        @JsonAlias("languages") List lenguajes,
+        @JsonAlias("download_count") Integer descargas
+        ) {
+}
