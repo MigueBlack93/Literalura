04289cfeedea9756b8c58827167e417e547f8cc2
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-10 17:03:33 -0300

    Update ConexionApi.java

--- src/main/java/com/miguel/literalura/service/ConexionApi.java
+++ src/main/java/com/miguel/literalura/service/ConexionApi.java
@@ -1,5 +1,7 @@
 package com.miguel.literalura.service;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,7 +11,17 @@ public class ConexionApi {
 
     public String conectarApi (String tipo){
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().build();
-        HttpResponse response =
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + tipo))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String json = response.body();
+        return json;
     }
 }
