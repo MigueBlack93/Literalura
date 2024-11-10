51545c59253244afbc7fc4e1ca42c74d63e2b384
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-10 19:48:11 -0300

    Update ConvierteDatos.java

--- src/main/java/com/miguel/literalura/service/ConvierteDatos.java
+++ src/main/java/com/miguel/literalura/service/ConvierteDatos.java
@@ -1,5 +1,7 @@
 package com.miguel.literalura.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ConvierteDatos implements IConvierteDatos {
     private ObjectMapper objectMapper = new ObjectMapper();
     @Override
