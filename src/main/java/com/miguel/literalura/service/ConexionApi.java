9fab1e1f649bc076c83caffc02af0a1d24febabd
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-10 16:38:25 -0300

    Create ConexionApi.java

---
+++ src/main/java/com/miguel/literalura/service/ConexionApi.java
@@ -0,0 +1,15 @@
+package com.miguel.literalura.service;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConexionApi {
+    private final String url = "https://gutendex.com/books";
+
+    public String conectarApi (String tipo){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().build();
+        HttpResponse response = 
+    }
+}
