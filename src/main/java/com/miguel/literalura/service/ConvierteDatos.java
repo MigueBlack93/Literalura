b0eaf5da7aa1b602a795500e8537f0bd09d8d4dc
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-13 22:27:26 -0300

    Update ConvierteDatos.java

--- src/main/java/com/miguel/literalura/service/ConvierteDatos.java
+++ src/main/java/com/miguel/literalura/service/ConvierteDatos.java
@@ -1,11 +1,17 @@
 package com.miguel.literalura.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConvierteDatos implements IConvierteDatos {
     private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public <T> T obtenerDatos(String json, Class<T> clase) {
-        return null;
+        try {
+            return objectMapper.readValue(json,clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
