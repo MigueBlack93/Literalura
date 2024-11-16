af1ae6f5a46b5a45acf1e5332eebf2a75e0d1858
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-16 08:31:35 -0300

    Update LiteraluraApplication.java

--- src/main/java/com/miguel/literalura/LiteraluraApplication.java
+++ src/main/java/com/miguel/literalura/LiteraluraApplication.java
@@ -1,5 +1,6 @@
 package com.miguel.literalura;
 
+import com.miguel.literalura.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class LiteraluraApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		Main main = new Main();
+		main.menu();
 	}
 }
