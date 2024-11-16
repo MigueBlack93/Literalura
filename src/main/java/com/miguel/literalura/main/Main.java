08a1333ffbd622d89cbbae664ee25f3640aaad6b
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-16 08:06:09 -0300

    Create Main.java

---
+++ src/main/java/com/miguel/literalura/main/Main.java
@@ -0,0 +1,88 @@
+package com.miguel.literalura.main;
+
+import com.miguel.literalura.model.Autores;
+import com.miguel.literalura.model.DatosLibro;
+import com.miguel.literalura.model.Libro;
+import com.miguel.literalura.service.ConexionApi;
+import com.miguel.literalura.service.ConvierteDatos;
+
+import java.util.Scanner;
+
+public class Main {
+    private Scanner teclado = new Scanner(System.in);
+    private ConexionApi conexionApi = new ConexionApi();
+    private ConvierteDatos convierteDatos = new ConvierteDatos();
+    private Libro libro = new Libro();
+    private Autores autores = new Autores();
+
+    public void menu(){
+        boolean salir = false;
+        int opcion;
+        Scanner teclado = new Scanner(System.in);
+
+        while (!salir){
+            System.out.println("""
+                    1 - Buscar Libro por nombre o por autor.
+                    2 - Mostrar libros registrados.
+                    3 - Mostrar autores registrados.
+                    4 - Mostrar autores vivos en un período determinado.
+                    5 - Mostar libros por idioma.
+                    
+                    0 - SALIR.
+                    """);
+            opcion = teclado.nextInt();
+
+            switch (opcion){
+                case 1:
+                    buscarLibro();
+                    break;
+                case 2:
+                    mostrarLibrosRegistrados();
+                    break;
+                case 3:
+                    mostrarAutoresRegistrados();
+                    break;
+                case 4:
+                    mostrarAutoresRegistradosPorPeriodo();
+                    break;
+                case 5:
+                    mostrarLibrosPorIdioma();
+                    break;
+                case 0:
+                    salir = true;
+                    System.out.println("""
+                            Muchas gracias por utilizar nuestro programa.
+                            ¡Hasta pronto!""");
+                    break;
+                default:
+                    System.out.println("""
+                            ¡Opción errónea!
+                            Po favor elija una opción del 1 al 5, o 0 para salir.""");
+            }
+        }
+    }
+
+    private DatosLibro getDatosLibro(){
+        System.out.println("Ingresa el nombre del libro o el autor que quieres buscar:");
+        var nombreABuscar = teclado.nextLine();
+        var json = conexionApi.conectarApi(nombreABuscar);
+        DatosLibro datosLibro = convierteDatos.obtenerDatos(json, DatosLibro.class);
+        return datosLibro;
+    }
+
+    private void buscarLibro() {
+        
+    }
+
+    private void mostrarLibrosRegistrados() {
+    }
+
+    private void mostrarAutoresRegistrados() {
+    }
+
+    private void mostrarAutoresRegistradosPorPeriodo() {
+    }
+
+    private void mostrarLibrosPorIdioma() {
+    }
+}
