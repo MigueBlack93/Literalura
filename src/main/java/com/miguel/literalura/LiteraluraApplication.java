b189afd04b09f1f13f1ad94647044cbf2529478b
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-18 10:45:38 -0300

    Update LiteraluraApplication.java

--- src/main/java/com/miguel/literalura/LiteraluraApplication.java
+++ src/main/java/com/miguel/literalura/LiteraluraApplication.java
@@ -1,6 +1,8 @@
 package com.miguel.literalura;
 
 import com.miguel.literalura.main.Main;
+import com.miguel.literalura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
+	@Autowired
+	private LibroRepository repository;
+
 	public static void main (String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Main main = new Main();
+		Main main = new Main(repository);
 		main.menu();
 	}
 }
