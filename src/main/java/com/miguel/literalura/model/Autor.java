60f648029a74230bd17f92969dd80abbbaf9ef23
Migue <miguelulmi@hotmail.com>
2024-11-19 11:27:11 -0300

    Create Autor.java

---
+++ src/main/java/com/miguel/literalura/model/Autor.java
@@ -0,0 +1,49 @@
+package com.miguel.literalura.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Integer nacimiento;
+    private Integer deceso;
+//    private Libro libro;
+
+    public Autor(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(Integer nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public Integer getDeceso() {
+        return deceso;
+    }
+
+    public void setDeceso(Integer deceso) {
+        this.deceso = deceso;
+    }
+}
