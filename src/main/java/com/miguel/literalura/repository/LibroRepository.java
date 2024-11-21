ddda0ae843e2f50160c62e0f720332ca47c4dba9
Migue <miguelulmi@hotmail.com>
2024-11-21 10:54:15 -0300

    Update LibroRepository.java

--- src/main/java/com/miguel/literalura/repository/LibroRepository.java
+++ src/main/java/com/miguel/literalura/repository/LibroRepository.java
@@ -3,6 +3,8 @@ package com.miguel.literalura.repository;
 import com.miguel.literalura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LibroRepository extends JpaRepository<Libro, Long>{
+import java.util.List;
 
+public interface LibroRepository extends JpaRepository<Libro, Long>{
+    List<Libro> buscarLibroPorIdioma(String idioma);
 }
