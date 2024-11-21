bddfe8f3e44a3a6aabaddb8ba87b48685ba9ea6b
Migue <miguelulmi@hotmail.com>
2024-11-21 10:54:13 -0300

    Update AutorRepository.java

--- src/main/java/com/miguel/literalura/repository/AutorRepository.java
+++ src/main/java/com/miguel/literalura/repository/AutorRepository.java
@@ -3,5 +3,9 @@ package com.miguel.literalura.repository;
 import com.miguel.literalura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+    List<Autor> buscarAutoresRegistradosEnUnPeriodo(int fechaInicial, int fechaFinal);
 }
