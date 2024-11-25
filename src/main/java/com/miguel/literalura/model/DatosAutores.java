8b7be6e94ee511876bdfb57aca97950bbda350cf
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-25 19:52:17 -0300

    Update DatosAutores.java

--- src/main/java/com/miguel/literalura/model/DatosAutores.java
+++ src/main/java/com/miguel/literalura/model/DatosAutores.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosAutores(
         @JsonAlias("name") String nombre,
-        @JsonAlias("birth_year") int nacimiento,
-        @JsonAlias("death_year") int deceso
+        @JsonAlias("birth_year") Integer nacimiento,
+        @JsonAlias("death_year") Integer deceso
 ) {
 }
