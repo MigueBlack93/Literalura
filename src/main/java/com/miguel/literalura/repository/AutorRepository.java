32b87a876b52f0571ed97c736b076af07031a5a0
Migue <101154043+MigueBlack93@users.noreply.github.com>
2024-11-25 19:52:25 -0300

    Update AutorRepository.java

--- src/main/java/com/miguel/literalura/repository/AutorRepository.java
+++ src/main/java/com/miguel/literalura/repository/AutorRepository.java
@@ -2,10 +2,12 @@ package com.miguel.literalura.repository;
 
 import com.miguel.literalura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
-    List<Autor> buscarAutoresRegistradosEnUnPeriodo(int fechaInicial, int fechaFinal);
+    //List<Autor> buscarAutoresRegistradosEnUnPeriodo(Integer fechaInicial, Integer fechaFinal);
 }
